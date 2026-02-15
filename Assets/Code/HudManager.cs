e869213a8971188f7e454bf4ff5c571eb2cea2d8
manello <manuel@baeuerle.email>
2026-02-15 19:33:53 +0100

    enemies now pause after an attack. Blinking sanity on hit WIP

--- Assets/Code/HudManager.cs
+++ Assets/Code/HudManager.cs
@@ -16,13 +16,40 @@ public class HudManager : MonoBehaviour
     GameObject HeldItemObj;
     Image HeldItem;
 
+
+    [SerializeField]
+    private float BlinkDuration = 1.5f;
+    [SerializeField]
+    private float BlinkInterval = 0.33f;
+    private float CurrentPercentage;
+    private bool Blink;
+    private bool HealBlink;
+    private float BlinkTimer;
+
     public void Start() 
     {
+        Blink = false;
+        CurrentPercentage = 1.0f;
         HeldItem = HeldItemObj.GetComponent<Image>();
+    }
+
+    public void Update() 
+    {
+        if (Blink)
+        {
+            // get blink color
+            // Get blink timer
+        }
     }
 
     public void SetSanityBar(float percentage)
     {
+        Blink = true;
+        if (percentage < CurrentPercentage) 
+            HealBlink = false;
+        else
+            HealBlink = true;
+        
         int filling = (int)Mathf.Round((SanityBarLevels.Count - 1) * percentage);
         
         foreach (GameObject obj in SanityBarLevels)
